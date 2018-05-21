1d007a861831e35c8c50eeb36c666994b0b7707e
Jeremy Murrah <murrahjm@gmail.com>
2018-05-21 07:07:20 -0500

    Update MSFT_DFSReplicationGroup for null compare-object error
    Potentially solves #27.  Add section to check for null values in existing member list or proposed member list before using compare-object.  Also added same logic to the comparison of existing and proposed folder list.

--- Modules/DFSDsc/DSCResources/MSFT_DFSReplicationGroup/MSFT_DFSReplicationGroup.psm1
+++ Modules/DFSDsc/DSCResources/MSFT_DFSReplicationGroup/MSFT_DFSReplicationGroup.psm1
@@ -626,10 +626,12 @@ function Test-TargetResource
 
             # Compare the Members
             $existingMembers = @((Get-DfsrMember @replicationGroupParameters -ErrorAction Stop).DnsName)
-            if ((Compare-Object `
-                -ReferenceObject $fqdnMembers `
-                -DifferenceObject $existingMembers).Count -ne 0)
-            {
+            #check for null values before using compare-object
+            # if one is null but not both then report difference
+            # if neither are null then compare values
+            if ([String]::IsNullOrEmpty($existingMembers) -and [String]::IsNullOrEmpty($fqdnMembers)) {
+                #both are null so no difference found
+            } elseif ([String]::IsNullOrEmpty($existingMembers) -xor [String]::IsNullOrEmpty($fqdnMembers)) {
                 # There is a member different of some kind.
                 Write-Verbose -Message ( @(
                     "$($MyInvocation.MyCommand): "
@@ -638,15 +640,31 @@ function Test-TargetResource
                     ) -join '' )
 
                 $desiredConfigurationMatch = $false
-            } # if
+            }  else {
+                #neither variables are null so it's safe to use compare-object
+                if ((Compare-Object `
+                    -ReferenceObject $fqdnMembers `
+                    -DifferenceObject $existingMembers).Count -ne 0)
+                {
+                    # There is a member different of some kind.
+                    Write-Verbose -Message ( @(
+                        "$($MyInvocation.MyCommand): "
+                        $($LocalizedData.ReplicationGroupMembersNeedUpdateMessage) `
+                            -f $GroupName
+                        ) -join '' )
+
+                    $desiredConfigurationMatch = $false
+                } # if
+            }
 
             # Compare the Folders
             $existingFolders = @((Get-DfsReplicatedFolder @replicationGroupParameters -ErrorAction Stop).FolderName)
-
-            if ((Compare-Object `
-                -ReferenceObject $Folders `
-                -DifferenceObject $existingFolders).Count -ne 0)
-            {
+            #check for null values before using compare-object
+            # if one is null but not both then report difference
+            # if neither are null then compare values
+            if ([String]::IsNullOrEmpty($existingFolders) -and [String]::IsNullOrEmpty($Folders)){
+                #both are null so no difference found
+            } elseif ([String]::IsNullOrEmpty($existingFolders) -xor [String]::IsNullOrEmpty($Folders)) {
                 # There is a folder different of some kind.
                 Write-Verbose -Message ( @(
                     "$($MyInvocation.MyCommand): "
@@ -655,8 +673,22 @@ function Test-TargetResource
                     ) -join '' )
 
                 $desiredConfigurationMatch = $false
-            } # if
+            } else {
+                #neither variables are null so it's safe to use compare-object
+                if ((Compare-Object `
+                    -ReferenceObject $Folders `
+                    -DifferenceObject $existingFolders).Count -ne 0)
+                {
+                    # There is a folder different of some kind.
+                    Write-Verbose -Message ( @(
+                        "$($MyInvocation.MyCommand): "
+                        $($LocalizedData.ReplicationGroupFoldersNeedUpdateMessage) `
+                            -f $GroupName
+                        ) -join '' )
 
+                    $desiredConfigurationMatch = $false
+                } # if
+            }
             # Get the content paths (if any were passed in the array)
             if ($ContentPaths)
             {
